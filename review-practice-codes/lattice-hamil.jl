ed18f1687af8599a3f077c70c06f4d57bacc8540
17thSaint <pgeraghty3@outlook.com>
2023-02-14 15:06:11 +0100

    trying to make number counter, not working

--- review-practice-codes/lattice-hamil.jl
+++ review-practice-codes/lattice-hamil.jl
@@ -49,6 +49,9 @@ function make_component_matrix(j,k,edge_count)
 		end
 		new_mat = kron(this_element,which_mat(which_mats[i1]))
 		series_of_prods[i1-1] = new_mat
+		if i1 > 2
+			series_of_prods[i1-2] = Matrix{Int64}(undef,(2,2))
+		end
 	end
 	return series_of_prods[end]
 end
@@ -56,7 +59,7 @@ end
 function make_ham_mat(edge_count,t,phi,spacing)
 	ham_mat = zeros(Int64,2^(edge_count^2),2^(edge_count^2))
 	for i2 in 1:edge_count^2
-		#println("Site $i2")
+		#println("Site $i2 / $edge_count")
 		for j2 in 1:edge_count^2
 			if j2 == i2
 				continue
@@ -65,37 +68,71 @@ function make_ham_mat(edge_count,t,phi,spacing)
 			coeff = get_j(j2,i2,t,phi,edge_count,spacing)
 			matrix_comp = make_component_matrix(j2,i2,edge_count)
 			ham_mat += coeff.*matrix_comp
+			#println(i2,", ",j2,", ",coeff)
+			#display(ham_mat)
 		end
 	end
 	
 	return eigvals(ham_mat),eigvecs(ham_mat),ham_mat
 end
 
+function get_total_number_particles(edge_count,wavefunc)
+	count = 0
+	for i in 1:edge_count^2
+		site_number_operator = make_component_matrix(i,i,edge_count)
+		multiplied_wavefunc = real.( (site_number_operator * wavefunc ) ./ wavefunc )
+		found_number = false
+		display(multiplied_wavefunc)
+		display(wavefunc)
+		element = 0
+		while !found_number
+			element += 1
+			local_number = multiplied_wavefunc[element] 
+			if !isnan(local_number) 
+				count += Int(round(local_number))
+				println("Found at Element $element: ",Int(round(local_number)),", Count is now $count")
+				found_number = true
+			else
+				println("Not Found at Element $element")
+			end
+		end
+	end
+	println("Final Count is $count")
+end
+
 lat_sep = 1.0
 t_val = 1.0
-edges_count = [2,3]
-phis = [1/3]
+edges_count = [2]
+phis_count = 1
+phis = [0.1 + (j-1)*(1.0-0.1)/phis_count for j in 1:phis_count]
 #phi_val = 1/3
 #
-percent_thru_eigvals = [[] for i in 1:length(edges_count)]
-energies = [[] for i in 1:length(edges_count)]
+#eivecs_here = Matrix{ComplexF64}(undef,(2^(edges_count[1]^2),2^(edges_count[1]^2)))
+percent_thru_eigvals = [[[] for j in 1:length(phis)] for i in 1:length(edges_count)]
+energies = [[[] for j in 1:length(phis)] for i in 1:length(edges_count)]
 for i in 1:length(edges_count)
 	num_edge_sites = edges_count[i]
 	for j in 1:length(phis)
+		#println(100*j/length(phis))
 		phi_val = phis[j]
-		en_vals,eivecs,mat = make_ham_mat(num_edge_sites,t_val,phi_val,lat_sep)
-		display(eivecs)
-		energies[i] = en_vals./maximum(en_vals)
-		percent_thru_eigvals[i] = [(k-1)/length(en_vals) for k in 1:length(en_vals)]
+		en_vals, eivecs, mat = make_ham_mat(num_edge_sites,t_val,phi_val,lat_sep)
+		for k in 1:1#size(eivecs)[1]
+			get_total_number_particles(num_edge_sites,eivecs[:,k])
+		end
+		#ham_mats = mat
+		#energies[i][j] = en_vals#./maximum(en_vals)
+		#percent_thru_eigvals[i][j] = [(k-1)/length(en_vals) for k in 1:length(en_vals)]
 	end
 end
-#
+#=
 for i in 1:length(edges_count)
 	edges = edges_count[i]
-	plot(percent_thru_eigvals[i],energies[i],"-p",label="$edges")
+	plot(phis,[energies[i][j][1] for j in 1:length(phis)],"-p",label="$edges")
 end
 legend()
-
+xlabel("Phi")
+ylabel("Lowest Energy")
+=#
 
 #=
 lat_sep = 1.0
