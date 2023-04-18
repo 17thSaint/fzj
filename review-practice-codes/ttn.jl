cc6159774836f20c11af0c4387765b22d91a68dc
17thSaint <pgeraghty3@outlook.com>
2023-04-18 13:25:49 +0200

    building hofstadter hamilt, linear_ind issues again

--- review-practice-codes/ttn.jl
+++ review-practice-codes/ttn.jl
@@ -110,20 +110,57 @@ function get_current_yfunc(edge_length,ttn; kwargs...)
 end
 
 
+function get_hofstadter_interacting_hamilt(edge_length,u_strength,t_strength,phi; kwargs...)
+	onsite = TTNKit.OpSum()
+	interaction = TTNKit.OpSum()
+	if_periodic = get(kwargs, :if_periodic, true)
+	for x in 1:edge_length
+		x_upper = x+1
+		if if_periodic && x == edge_length
+			x_upper = 1
+		end
+		for y in 1:edge_length
+			y_upper = y+1
+			if if_periodic && y == edge_length
+				y_upper = 1
+			end
+			site_number = (get_site_number(x,y,edge_length))
+			onsite += (u_strength,"N",site_number,"N - Id",site_number)
+			
+			if x == edge_length && !if_periodic
+				continue
+			else
+				next_x = (get_site_number(x_upper,y,edge_length))
+				next_y = (get_site_number(x,y_upper,edge_length))
+				interaction += (-t_strength,"Adag",next_x,"A",site_number)
+				interaction += (-t_strength*exp(im*2*pi*phi*x),"Adag",site_number,"A",next_y)
+				
+				interaction += (-t_strength,"Adag",site_number,"A",next_x)
+				interaction += (-t_strength*exp(-im*2*pi*phi*x),"Adag",next_y,"A",site_number)
+			end
+		end
+	end
+	return onsite + interaction
+end
+
 edge_sites = 4
+num_particles = 5
 maxdim = 2
 num_sweeps = 1
 noise = 0.0
 
-if false
+if true
 
-square = TTNKit.BinaryNetwork((edge_sites,edge_sites), TTNKit.ITensorNode, "SpinHalf")
+square = TTNKit.BinaryNetwork((edge_sites,edge_sites), TTNKit.ITensorNode, "Boson")
 lat = TTNKit.physical_lattice(square)
 num_sites = length(lat)
 println("Finished Building Network")
 #sh = TTNKit.SimpleSweepHandler(chain,num_sweeps)
 
-states = fill("Dn",num_sites)
+states = fill("0",num_sites)
+for i in 1:num_particles
+	states[rand((1:num_sites))] = "1"
+end
 #states[Int(ceil(num_sites/2))] = "Dn"
 ttn = TTNKit.ProductTreeTensorNetwork(square,states;orthogonalize=true)
 ttn = TTNKit.increase_dim_tree_tensor_network_zeros(ttn, maxdim = maxdim)
@@ -141,22 +178,27 @@ func = (action,T) -> TTNKit.eigsolve(action, T, 1,
                             krylovdim=eigsolve_krylovdim,
                             maxiter=eigsolve_maxiter)
 
-
+#=
 js, gs = -1.0, -2.0
 ising = TTNKit.TransverseFieldIsing(J = js, g = gs)
 tpo = TTNKit.Hamiltonian(ising,lat)
 proj_tpo = TTNKit.ProjectedTensorProductOperator(ttn,tpo)
 println("Finished Making Hamiltonian")
 
+
 sp = TTNKit.SimpleSweepHandler(ttn,proj_tpo,func,num_sweeps,[maxdim],[noise],TTNKit.NoExpander())
 TTNKit.sweep(ttn,sp);
-
+=#
 end
 
-rez = get_current_yfunc(edge_sites,ttn)
+#rez = get_current_yfunc(edge_sites,ttn)
 
-
-
+us = 1.0
+ts = 1.0
+filling = 1/3
+mixing = num_particles/(filling * (edge_sites^2))
+hofham = get_hofstadter_interacting_hamilt(edge_sites,us,ts,mixing)
+tpo = TTNKit.TPO(hofham,lat)
 
 
 
