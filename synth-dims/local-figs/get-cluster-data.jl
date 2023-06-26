ae2fc86b11a8102423cd6fabc7be68281cd86dcf
@pgeraghty <pgeraghty3@outlook.com>
2023-06-26 17:29:22 +0200

    end of day

--- synth-dims/local-figs/get-cluster-data.jl
+++ synth-dims/local-figs/get-cluster-data.jl
@@ -37,85 +37,27 @@ end
 
 layers = 6
 mdim = 150
-lr = 1
+lr = 0
 #
-if false
-params_magOFF = Dict([("lr",lr),("layers",layers),("mdim",mdim),("mag_off",true)])
-all_ttns_magOFF,all_files_magOFF = get_ttns(params_magOFF)
-lr0params_magOFF = Dict([("lr",0),("layers",layers),("mdim",mdim),("mag_off",true),("nn_strength",0.0)])
-lr0_ttn_magOFF = get_ttns(lr0params_magOFF)
-append!(all_ttns_magOFF,[lr0_ttn_magOFF[1][1]])
-append!(all_files_magOFF,[lr0_ttn_magOFF[2][1]])
-end
-if false
-params_magON = Dict([("lr",lr),("layers",layers),("mdim",mdim),("mag_off",false)])
-all_ttns_magON,all_files_magON = get_ttns(params_magON)
-lr0params_magON = Dict([("lr",0),("layers",layers),("mdim",mdim),("mag_off",false),("nn_strength",0.0)])
-lr0_ttn_magON = get_ttns(lr0params_magON)
-append!(all_ttns_magON,[lr0_ttn_magON[1][1]])
-append!(all_files_magON,[lr0_ttn_magON[2][1]])
-end
-
-#=
-dist1_corrs_off = []
-strengths_off = []
 if true
-for i in 1:length(all_ttns_magOFF)
-	println(i)
-	ttn = all_ttns_magOFF[i]["ttn"]
-	filename = all_files_magOFF[i]
-	stren = get_params_dict_from_filename(filename)["nn_strength"]
-	title_string = "NN Str = $(stren), Mdim=$mdim, Mag OFF, Np=4"
-	append!(strengths_off,[stren])
-	rez = get_allAVG_densdenscorr(ttn,[i for i in 1:2*Int(sqrt(2^layers))]; plot_title=title_string,if_save_fig=false,if_plot=false,location=pwd(),name=filename)
-	append!(dist1_corrs_off,[rez[1][1]])
-	#get_occupancy(ttn; plot_title=title_string,if_save_fig=false,location=pwd(),name=filename)
+params = Dict([("lr",lr),("layers",layers),("mdim",mdim),("mag_off",false)])
+all_ttns,all_files = get_ttns(params)
 end
-scatter(strengths_off,dist1_corrs_off,label="Mag OFF")
-end
-=#
 
 
 #
-bds_on = zeros(3,length(all_ttns_magOFF))
-strengths_on = []
-if true
-for j in [1,2,3]
-for i in 1:length(all_ttns_magOFF)
+if false
+for i in 1:Int(length(all_ttns)/3)
 	println(i)
-	ttn = all_ttns_magOFF[i]["ttn"]
+	ttn_neg = all_ttns[3*(i-1)+1]["ttn"]
+	ttn_mid = all_ttns[3*(i-1)+2]["ttn"]
+	ttn_pos = all_ttns[3*(i-1)+3]["ttn"]
 	filename = all_files_magOFF[i]
-	if j == 1
-		stren = get_params_dict_from_filename(filename)["nn_strength"]
-		#title_string = "NN Str = $(stren), Mdim=$mdim, Mag ON, Np=4"
-		append!(strengths_on,[stren])
-	end
+	forward_deriv = deriv_bulk_dens(ttn_pos,ttn_mid,0.0001,1)
+	back_deriv = deriv_bulk_dens(ttn_mid,ttn_neg,-0.0001,1)
 	bds_on[j,i] = bulk_density(ttn,j)
 end
-scatter(strengths_on,bds_on[j,:],label="Width=$j")
-legend()
 end
-xlabel("NN Strength")
-ylabel("Bulk Density")
-title("Bulk Density range Bulk Size, Mag OFF, Ns=64")
-end
-
-#=
-on_poses = []
-for i in 1:length(all_ttns_magON)
-	ttn = all_ttns_magON[i]["ttn"]
-	append!(on_poses,[bulk_density(ttn)])
-end
-scatter(strengths,on_poses)
-
-off_poses = []
-for i in 1:length(all_ttns_magOFF)
-	ttn = all_ttns_magOFF[i]["ttn"]
-	append!(off_poses,[bulk_density(ttn)])
-end
-scatter(strengths,off_poses)
-=#
-
 
 
 
