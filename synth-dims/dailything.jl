741aaaff7f2462569cd4a5ae16308a05071dc3d1
@pgeraghty <pgeraghty3@outlook.com>
2023-10-04 09:13:19 +0200

    added metadata dictionaries to everything about to move all files to regular cluster data folder

--- synth-dims/dailything.jl
+++ synth-dims/dailything.jl
@@ -1,5 +1,6 @@
 include("long-range-ttn.jl")
 include("fqh_effective.jl")
+using PyPlot
 
 alpha_start = 0.251
 alpha_end = 0.501
@@ -12,18 +13,79 @@ bulkdenses = [0.0 for i in 1:3*alpha_count]
 derivbds = [0.0 for i in 1:alpha_count]
 
 filename1 = "mps-if_periodic_phys-false-nflavors-5-alpha-0.125-if_periodic_synth-true-nbosons-60-L-96.jld2"
-filename2 = "mps-if_periodic_phys-false-nflavors-5-alpha-0.25-if_periodic_synth-true-nbosons-60-L-96.jld2"
+#filename2 = "mps-if_periodic_phys-false-nflavors-5-alpha-0.25-if_periodic_synth-true-nbosons-60-L-96.jld2"
 #seed_data1 = read_data_jld2(filename1,"local-figs/orsay-sept23")
 #seed_data2 = read_data_jld2(filename2,"local-figs/orsay-sept23")
 
+cd("local-figs/orsay-sept23")
+
+if false
+all_files = readdir()
+deleteat!(all_files,findall(i->split(all_files[i],"-")[1] != "mps",1:length(all_files)))
+display(all_files)
+end
+
+to_remove = []
+if false
+for (idx,f) in enumerate(all_files)
+	file = jldopen(f, "r")
+	if length(keys(file)) > 1
+		append!(to_remove,[idx])
+	end
+	close(f)
+end
+deleteat!(all_files,to_remove)
+display(all_files)
+end
+
+if false
+for (idx,f) in enumerate(all_files)
+	name_dict = get_params_dict_from_filename(f)
+	if name_dict["nbosons"] != 60
+		append!(to_remove,[idx])
+	end
+end
+deleteat!(all_files,to_remove)
+display(all_files)
+end
+
+if true
+for (idx,f) in enumerate(all_files)
+	name_dict = get_params_dict_from_filename(f)
+	new_metadata = merge(ogmetadata,name_dict)
+	new_metadata["phi"] = new_metadata["alpha"] * 2 * pi
+	file = jldopen(f,"a+")
+	#display(keys(file))
+	try
+		metadata_var = JLD2.Group(file,"metadata")
+		for metadatum_key in keys(new_metadata)
+			metadata_var[metadatum_key] = new_metadata[metadatum_key]
+		end
+		close(file)
+	catch
+		println("Didnt work on file ",f)
+		close(file)
+	end
+	
+end
+end
+
+
+
+cd("../..")
+
+
+
+
+#=
 seed_data = seed_data1
 seed_data[1] = seed_data2
 
 for i in 1:alpha_count
 	println(i/alpha_count)
 	alph = alpha_vals[i]
-	psi_mid = run_mps_new_variable(seed_data[1]["mps"],seed_data[2],Dict([("phi",2*pi*alph),("typechange","nothing")]))
-	psi_m,psi_p = varied_alpha_wavefuncs(psi_mid,merge(seed_data[2],Dict([("phi",2*pi*alph)])),change)
+	psi_mid = run_mps_new_variable(seed_data[1]["mps"],seed_data[2],Dict([("phi",2*pi*alph),("typechange","nothing"),("mdim",200)]))
+	psi_m,psi_p = varied_alpha_wavefuncs(psi_mid,merge(seed_data[2],Dict([("phi",2*pi*alph),("mdim",200)])),change)
 	wavefuncs[string(round(alph,digits=4))] = [psi_m,psi_mid,psi_p]
 	bd_m = bulk_density(psi_m,10)
 	bulkdenses[3*(i-1)+1] = bd_m
@@ -43,7 +105,7 @@ plot(alpha_vals,derivbds,"-p")
 #plot(Iterators.flatten([[alpha_vals[i]-change,alpha_vals[i],alpha_vals[i]+change] for i in 1:alpha_count]),bulkdenses,"-p")
 #title("Bulk Density vs Flux Density")
 
-
+=#
 # Work looking at more flux values, 0.25 has an oddly high bond dimension....
 
 
