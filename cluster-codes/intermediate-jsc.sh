99dfe48fc416938bb663213fb44fd90da54abbc8
@pgeraghty <pgeraghty3@outlook.com>
2023-10-24 14:03:06 +0200

    execution files juwels ready

--- cluster-codes/intermediate-jsc.sh
+++ cluster-codes/intermediate-jsc.sh
@@ -6,7 +6,7 @@ STEP_SIZE=$3
 datafolder=$4
 additional_params=("${@:5}")
 
-value=$(( ( $SLURM_ARRAY_TASK_ID - 1 ) * $STEP_SIZE + $START_VALUE ))
+value=$(bc -l <<< "($SLURM_ARRAY_TASK_ID - 1) * $STEP_SIZE + $START_VALUE" )
 echo "$value"
 
 ./run-script-jsc.sh "$script_name" "open_cores" 4 "dataloc" "$datafolder" "$param" "$value" "${additional_params[@]}"
--- cluster-codes/jsc-sbatch.sh
+++ cluster-codes/jsc-sbatch.sh
@@ -9,10 +9,12 @@
 echo "Date:"
 date
 echo "Nodes used:"
+
 echo $SLURM_NODELIST
 export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
 
 #module load CUDA/11.5 cuDNN/8.3.1.22-CUDA-11.5 Intel/2021.4.0 ParaStationMPI mpi4py h5py
+
 module load Julia
 
 #source # !! HIER DEIN ENVIRONMENT AKTIVIEREN 
@@ -20,13 +22,12 @@ module load Julia
 #export XLA_FLAGS="--xla_gpu_cuda_data_dir=$CUDA_HOME"
 #export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-
 # Get the script name, parameter name, start value, end value, and step size
 script_name=$1
 param=$2
 START_VALUE=$3
 END_VALUE=$4
-STEP_SIZE=$(( ( $END_VALUE - $START_VALUE ) / ( $SLURM_ARRAY_TASK_COUNT - 1.0 ) ))
+STEP_SIZE=$(bc -l <<< "($END_VALUE - $START_VALUE) / ($SLURM_ARRAY_TASK_COUNT - 1)")
 additional_params=("${@:5}")
 
 # Calculate the number of iterations
@@ -40,8 +41,8 @@ datafolder="/p/project/netenesyquma/geraghty1/data/data-$alpha"
 mkdir -p "$datafolder"
 
 
-value=$(( ( $SLURM_ARRAY_TASK_ID - 1 ) * $STEP_SIZE + $START_VALUE ))
-echo "$value"
+#value=$(( ( $SLURM_ARRAY_TASK_ID - 1 ) * $STEP_SIZE + $START_VALUE ))
+#echo "$value"
 
 srun intermediate-jsc.sh "$param" "$START_VALUE" "$STEP_SIZE" "$datafolder" "${additional_params[@]}"
 
