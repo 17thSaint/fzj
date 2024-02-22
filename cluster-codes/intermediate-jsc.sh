4d99860ec03297f41b6d0b09b25bd92b1ba36685
@pgeraghty <pgeraghty3@outlook.com>
2024-02-22 13:47:00 +0100

    jsc running scripts updated

--- cluster-codes/intermediate-jsc.sh
+++ cluster-codes/intermediate-jsc.sh
@@ -7,7 +7,7 @@ STEP_SIZE=$4
 datafolder=$5
 additional_params=("${@:6}")
 
-value=$(bc -l <<< "($SLURM_ARRAY_TASK_ID - 1) * $STEP_SIZE + $START_VALUE" )
+value=$(bc -l <<< "($SLURM_PROCID) * $STEP_SIZE + $START_VALUE" )
 echo "$value"
 
 ./run-script-jsc.sh "$script_name" "dataloc" "$datafolder" "$param" "$value" "${additional_params[@]}"
--- cluster-codes/jsc-sbatch.sh
+++ cluster-codes/jsc-sbatch.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -x
 
-#SBATCH --time=00:30:00
-#SBATCH --cpus-per-task=1
-#SBATCH --tasks-per-node=10
-#SBATCH --account=pgi-8
+#SBATCH --account=netenesyquma
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --ntasks-per-node=10
+#SBATCH --time=00:10:00
+#SBATCH --partition=batch
 
 echo "Date:"
 date
 echo "Nodes used:"
 
-echo $SLURM_NODELIST
 export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
 
 #module load CUDA/11.5 cuDNN/8.3.1.22-CUDA-11.5 Intel/2021.4.0 ParaStationMPI mpi4py h5py
@@ -26,7 +27,7 @@ script_name=$1
 param=$2
 START_VALUE=$3
 END_VALUE=$4
-STEP_SIZE=$(bc -l <<< "($END_VALUE - $START_VALUE) / ($SLURM_ARRAY_TASK_COUNT - 1)")
+STEP_SIZE=$(bc -l <<< "($END_VALUE - $START_VALUE) / ($SLURM_NTASKS_PER_NODE * $SLURM_JOB_NUM_NODES - 1)")
 additional_params=("${@:5}")
 
 # Calculate the number of iterations
@@ -34,7 +35,7 @@ additional_params=("${@:5}")
 
 # Get the current date and time
 #alpha=$(date +"%Y-%m-%d_%H-%M")
-datafolder="/p/project/netenesyquma/geraghty1/data/data-zoomed-chemstrength"
+datafolder="/p/project/netenesyquma/geraghty1/data/data-$alpha"
 
 # Create the folder
 #mkdir -p "$datafolder"
