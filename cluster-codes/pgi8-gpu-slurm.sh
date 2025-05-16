8e008d66d34121eb8afb71fd51a34282570fb0b6
@pgeraghty <pgeraghty3@outlook.com>
2025-05-16 11:50:09 +0200

    all ready for niklas

--- cluster-codes/pgi8-gpu-slurm.sh
+++ cluster-codes/pgi8-gpu-slurm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=gpu_withexpander_16x8
-#SBATCH --output=log_file
-#SBATCH --error=log_file
+#SBATCH --job-name=gpu_10x5_pinning_ulr
+#SBATCH --output=log_file_10x5_pinning_ulr
+#SBATCH --error=log_file_10x5_pinning_ulr
 #SBATCH -p pgi-8-gpu-h100
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
 #SBATCH --mem=128G
 #SBATCH --gres=gpu:h100:1
 
-srun julia long-range-ttn.jl "Lx" 10 "Ly" 5 "particles" 5 "onsite_strength" 0.0 "mdim" 300 "pinning_strength" 0.1 
+srun julia long-range-ttn.jl "Lx" 10 "Ly" 5 "particles" 5 "onsite_strength" 300.0 "mdim" 300 "pinning_strength" 0.001
